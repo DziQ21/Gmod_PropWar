f4b1eb452463108b87260aaa80eb0bbd21e23b4e
Pawel Radzik <pawel.radzik@interia.eu>
2019-01-19 11:07:29 +0100

    naprawiem pierwsze errory

--- Gamemode/init.lua
+++ Gamemode/init.lua
@@ -3,13 +3,14 @@ AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
 
-
 GM:PlayerConnect(name , ip)
 
+  print(name.."conected with ip "..ip)
 
 end
+
 GM:PlayerInitialSpawn(ply)
 
-
+  print(ply.."initial spawn")
 
 end
